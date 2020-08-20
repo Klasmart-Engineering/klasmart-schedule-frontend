41faf72040747e3253c0a46d77bd49d13c3f5042
james.jiang <james.jiang@badanamu.com.cn>
2020-08-20 11:59:04 +0800

    feat: react-big-schedule && layout

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import Calendar from "../../components/Calendar";
+import ScheduleTool from "./ScheduleTool";
+import ScheduleEdit from "./ScheduleEdit";
+import LayoutBox from "../../components/LayoutBox";
+import { Grid } from "@material-ui/core";
+
+function ScheduleContent() {
+  return (
+    <LayoutBox holderMin={40} holderBase={80} mainBase={1920}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <ScheduleTool />
+        </Grid>
+        <Grid item xs={3}>
+          <ScheduleEdit />
+        </Grid>
+        <Grid item xs={9}>
+          <Calendar />
+        </Grid>
+      </Grid>
+    </LayoutBox>
+  );
+}
 
 export default function Schedule() {
-  return <div>这是schedule组件</div>;
+  return <ScheduleContent />;
 }
