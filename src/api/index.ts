c1081e2f05dee17b3ce392b4df685c1ffa69ac0b
juanjuan.qin <juanjuan.qin@badanamu.com.cn>
2020-08-21 18:06:03 +0800

    feat: add support fetch intercept

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,24 @@
+import fetchIntercept from "fetch-intercept";
 import { Api } from "./api";
+
+fetchIntercept.register({
+  // request: function (url, config) {
+  //     // Modify the url or config here
+  //     return [url, config];
+  // },
+  // requestError: function (error) {
+  //     // Called when an error occured during another 'request' interceptor call
+  //     return Promise.reject(error);
+  // },
+  // response: function (response) {
+  //     // Modify the reponse object
+  //     return response;
+  // },
+  // responseError: function (error) {
+  //     // Handle an fetch error
+  //     return Promise.reject(error);
+  // }
+});
 
 export default new Api({
   baseUrl: process.env.REACT_APP_BASE_API,
