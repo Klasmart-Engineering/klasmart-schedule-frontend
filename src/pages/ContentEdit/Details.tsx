9bdf601882f6b8f6ad705045feb4b5ee0f51c88a
jacinto <ext.ruyue.liu@badanamu.com.cn>
2020-08-26 14:46:20 +0800

    feat:contentEdit 1 tabName madia Assets ,media Material 2 details add reject_Reson,

--- src/pages/ContentEdit/Details.tsx
+++ src/pages/ContentEdit/Details.tsx
@@ -1,12 +1,18 @@
 import { Box, Button, createMuiTheme, makeStyles, MenuItem, TextField, ThemeProvider, useMediaQuery, useTheme } from "@material-ui/core";
 import { CloudUploadOutlined } from "@material-ui/icons";
+import clsx from "clsx";
 import React, { useEffect } from "react";
 import { Controller, UseFormMethods } from "react-hook-form";
+import { useParams } from "react-router-dom";
 import { Content, CreateContentRequest } from "../../api/api";
 
 const useStyles = makeStyles(({ breakpoints, shadows, palette }) => ({
   fieldset: {
     marginTop: 20,
+  },
+  fieldsetDisabled: {
+    color: "red",
+    borderColor: "red",
   },
   halfFieldset: {
     marginTop: 20,
@@ -28,6 +34,7 @@ export default function Details(props: DetailsProps) {
     formMethods: { register, control, errors },
   } = props;
   const css = useStyles();
+  const { lesson } = useParams();
   const defaultTheme = useTheme();
   const sm = useMediaQuery(defaultTheme.breakpoints.down("sm"));
   const size = sm ? "small" : "medium";
@@ -52,7 +59,24 @@ export default function Details(props: DetailsProps) {
   return (
     <ThemeProvider theme={theme}>
       <Box component="form" p="7.8% 8.5%">
-        <Controller as={TextField} control={control} name="name" label="Material Name" defaultValue={contentDetail.name}></Controller>
+        {contentDetail.publish_status === "regected" && (
+          <TextField
+            label="Reject Resson"
+            error
+            disabled
+            color="secondary"
+            className={clsx(css.fieldset, css.fieldsetDisabled)}
+            defaultValue={contentDetail.reject_reason}
+          ></TextField>
+        )}
+        <Controller
+          as={TextField}
+          control={control}
+          className={css.fieldset}
+          name="name"
+          label={lesson === "material" ? "Material Name" : "Plan Name"}
+          defaultValue={contentDetail.name}
+        ></Controller>
         <Box className={css.fieldset}>
           <input id="thumbnail-file-input" type="file" accept="image/*" hidden></input>
           <label htmlFor="thumbnail-file-input">
