ab8e49c7b668ef9a3892dce8d1c4e666d1866a0c
Leyun <ext.leyun.zhu@badanamu.com.cn>
2020-08-27 17:22:26 +0800

    feat:调试getScheduleList接口

--- src/reducers/schedule.ts
+++ src/reducers/schedule.ts
@@ -13,7 +13,36 @@ interface Rootstate {
 
 const initialState: ScheduleState = {
   total: 0,
-  scheduleList: [],
+  scheduleList: [
+    {
+      id: "Math.floor(1000)",
+      title: "Zoo Animals",
+      start_at: 1597912763,
+      end_at: 1597916363,
+      lesson_plan: {
+        id: "Math.floor(1000)",
+        name: "Big Lesson Plan",
+      },
+      program: {
+        id: "11",
+        name: "someProgram",
+      },
+      subject: {
+        id: "111",
+        name: "someSubject",
+      },
+      class: {
+        id: "22",
+        name: "someClass",
+      },
+      teachers: [
+        {
+          id: "Math.floor(1000)",
+          name: "handsome teacher",
+        },
+      ],
+    },
+  ],
 };
 
 type querySchedulesParams = Parameters<typeof api.schedules.querySchedules>[0];
