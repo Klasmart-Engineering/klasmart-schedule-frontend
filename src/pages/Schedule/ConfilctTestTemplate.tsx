1f0a70554d63ec97cc889e4dda2f7954ec26b49a
Leyun <ext.leyun.zhu@badanamu.com.cn>
2020-09-01 18:05:12 +0800

    feat: add title and buttons for ConfilctTestTemplate.tsx

--- src/pages/Schedule/ConfilctTestTemplate.tsx
+++ src/pages/Schedule/ConfilctTestTemplate.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
@@ -8,17 +9,24 @@ export default function AlertDialog() {
 
   return (
     <div>
-      <FormControl component="fieldset">
-        <RadioGroup
-          aria-label="gender"
-          name="gender1"
-          value={radioValue}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setRadioValue(event.target.value)}
-        >
-          <FormControlLabel value="only_current" control={<Radio />} label="This event" />
-          <FormControlLabel value="with_following" control={<Radio />} label="This and following events" />
-        </RadioGroup>
-      </FormControl>
+      <p style={{ fontSize: "20px" }}>Edit recurring event</p>
+      <div style={{ paddingLeft: "20px" }}>
+        <FormControl component="fieldset">
+          <RadioGroup
+            aria-label="gender"
+            name="gender1"
+            value={radioValue}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setRadioValue(event.target.value)}
+          >
+            <FormControlLabel value="only_current" control={<Radio />} label="This event" />
+            <FormControlLabel value="with_following" control={<Radio />} label="This and following events" />
+          </RadioGroup>
+        </FormControl>
+      </div>
+      <div style={{ marginTop: "20px", textAlign: "right" }}>
+        <Button color="primary">Cancel</Button>
+        <Button color="primary">Delete</Button>
+      </div>
     </div>
   );
 }
