4fcca4f70b4b28c862c245a643277c88a75418fd
Cheryl Wang <cheryl.wang@badanamu.com.cn>
2020-09-14 10:54:34 +0800

    feat:contentPreview页assessments

--- src/pages/ContentPreview/Assessments.tsx
+++ src/pages/ContentPreview/Assessments.tsx
@@ -1,6 +1,11 @@
 import { Box } from "@material-ui/core";
 import React from "react";
+import { Comingsoon } from "../ContentEdit/MediaAssets";
 
 export function Assessments() {
-  return <Box style={{ height: 700 }}>Assessments</Box>;
+  return (
+    <Box style={{ height: 700, textAlign: "center" }}>
+      <Comingsoon />
+    </Box>
+  );
 }
