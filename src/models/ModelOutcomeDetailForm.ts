7dd4d3eda88b1bf0c3fa98f07d18b8f4bbd23c07
Leyun <ext.leyun.zhu@badanamu.com.cn>
2020-09-22 14:41:53 +0800

    feat: modify data when update a outcome

--- src/models/ModelOutcomeDetailForm.ts
+++ src/models/ModelOutcomeDetailForm.ts
@@ -5,21 +5,27 @@ export const modelOutcomeDetail = (outcomeDetail: ApiOutcomeView) => {
   const { program, subject, developmental, skills, age, grade } = outcomeDetail;
   if (program && program.length) {
     afterData.program = program.map((item: any) => item.program_id);
+    afterData.program = afterData.program.filter((item: string) => item);
   }
   if (subject && subject.length) {
     afterData.subject = subject.map((item: any) => item.subject_id);
+    afterData.subject = afterData.subject.filter((item: string) => item);
   }
   if (developmental && developmental.length) {
     afterData.developmental = developmental.map((item: any) => item.developmental_id);
+    afterData.developmental = afterData.developmental.filter((item: string) => item);
   }
   if (skills && skills.length) {
     afterData.skills = skills.map((item: any) => item.skill_id);
+    afterData.skills = afterData.skills.filter((item: string) => item);
   }
   if (age && age.length) {
     afterData.age = age.map((item: any) => item.age_id);
+    afterData.age = afterData.age.filter((item: string) => item);
   }
   if (grade && grade.length) {
     afterData.grade = grade.map((item: any) => item.grade_id);
+    afterData.grade = afterData.grade.filter((item: string) => item);
   }
   return afterData;
 };
