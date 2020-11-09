befe0ec7d56f1030c1ad4dc090a2479e6a81b3af
juanjuan.qin <juanjuan.qin@badanamu.com.cn>
2020-11-09 16:08:52 +0800

    refactor: syncLang

--- src/locale/lang/type.ts
+++ src/locale/lang/type.ts
@@ -407,7 +407,8 @@ type LangRecord<T = string> =
   | { id: "report_label_descending"; description: "Descending"; values: undefined }
   | { id: "report_label_learning_outcomes_percentage"; description: "Learning Outcomes %"; values: undefined }
   | { id: "report_label_absent"; description: "Absent"; values: undefined }
-  | { id: "general_error_unauthorized"; description: "User not logged in, please log in"; values: undefined };
+  | { id: "general_error_unauthorized"; description: "User not logged in, please log in"; values: undefined }
+  | { id: "general_error_no_organization"; description: "You have no organization"; values: undefined };
 
 export type LangRecordId = LangRecord["id"];
 export type LangRecodeDescription = LangRecord["description"];
