c267c8429fb785a201f882f30aa15e45732f001e
Cheryl Wang <cheryl.wang@badanamu.com.cn>
2020-12-08 16:19:53 +0800

    fix:assetsList 子文件夹为空时不显示返回上一层图标

--- src/pages/MyContentList/index.tsx
+++ src/pages/MyContentList/index.tsx
@@ -331,7 +331,9 @@ export default function MyContentList() {
                 onApprove={handleApprove}
                 onReject={handleReject}
               />
-            ) : JSON.stringify(parentFolderInfo) !== "{}" && condition.publish_status === PublishStatus.published ? (
+            ) : JSON.stringify(parentFolderInfo) !== "{}" &&
+              (condition.publish_status === PublishStatus.published ||
+                condition.content_type === SearchContentsRequestContentType.assetsandfolder) ? (
               <LayoutBox holderMin={40} holderBase={202} mainBase={1517}>
                 <BackToPrevPage onGoBack={handleGoback} parentFolderInfo={parentFolderInfo} total={total} />
               </LayoutBox>
