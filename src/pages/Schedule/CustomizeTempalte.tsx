df035cf05e2e94937f0445257e5dc97ab296a9de
james.jiang <james.jiang@badanamu.com.cn>
2020-12-24 11:27:19 +0800

    feat: fix error notice style

--- src/pages/Schedule/CustomizeTempalte.tsx
+++ src/pages/Schedule/CustomizeTempalte.tsx
@@ -14,7 +14,7 @@ import ContentPreview from "../ContentPreview";
 const useStyles = makeStyles({
   previewContainer: {
     width: "500px",
-    height: "260px",
+    maxHeight: "300px",
     borderRadius: "4px",
     boxShadow: "0px 11px 15px -7px rgba(0,0,0,0.2), 0px 9px 46px 8px rgba(0,0,0,0.12), 0px 24px 38px 3px rgba(0,0,0,0.14)",
     padding: "20px 30px",
@@ -60,6 +60,7 @@ const useStyles = makeStyles({
   checkPlan: {
     color: "#E02020",
     fontWeight: "bold",
+    paddingTop: "10px",
   },
 });
 
