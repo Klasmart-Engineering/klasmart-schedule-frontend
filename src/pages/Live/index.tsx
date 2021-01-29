26b2cf560bb202a78ae2f99181843b091eb68e17
juanjuan.qin <juanjuan.qin@badanamu.com.cn>
2021-01-29 13:37:30 +0800

    feat: 将 live 的url参数移到search里

--- src/pages/Live/index.tsx
+++ src/pages/Live/index.tsx
@@ -1,7 +1,6 @@
 import { Box } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router";
 import { EntityContentInfoWithDetails } from "../../api/api.auto";
 import { ModelLessonPlan, Segment } from "../../models/ModelLessonPlan";
 import { RootState } from "../../reducers";
@@ -9,15 +8,14 @@ import { onLoadContentPreview } from "../../reducers/content";
 import { H5pPlayerInline } from "./H5pPlayerInline";
 
 const useQuery = () => {
-  const { search } = useLocation();
-  const query = new URLSearchParams(search);
+  const query = new URLSearchParams(location.search);
   const content_id = query.get("content_id") as string;
   const schedule_id = query.get("schedule_id") as string;
   return { content_id, schedule_id };
 };
 export default function LiveH5p() {
   const { contentPreview, user_id } = useSelector<RootState, RootState["content"]>((state) => state.content);
-  const { content_id, schedule_id } = useQuery();
+  const { content_id: materialId, schedule_id } = useQuery();
   const [h5pContent, setH5pContent] = React.useState<string>("");
   const dispatch = useDispatch();
   const checkH5p = (h5pArray: (EntityContentInfoWithDetails | undefined)[]) => {
@@ -27,8 +25,8 @@ export default function LiveH5p() {
     if (res && res[0]) return JSON.parse(res[0].data);
   };
   React.useEffect(() => {
-    dispatch(onLoadContentPreview({ metaLoading: true, content_id: content_id, schedule_id: "" }));
-  }, [content_id, dispatch, schedule_id]);
+    dispatch(onLoadContentPreview({ metaLoading: true, content_id: materialId, schedule_id: "" }));
+  }, [materialId, dispatch, schedule_id]);
   React.useEffect(() => {
     if (!contentPreview.id) return;
     const segment: Segment = JSON.parse(contentPreview.data || "{}");
@@ -40,7 +38,7 @@ export default function LiveH5p() {
       if (h5pItem && h5pItem.content) setH5pContent(h5pItem.content);
     }
   }, [contentPreview]);
-  return <Box>{h5pContent && <H5pPlayerInline valueSource={h5pContent} id={content_id} scheduleId={schedule_id} userId={user_id} />}</Box>;
+  return <Box>{h5pContent && <H5pPlayerInline valueSource={h5pContent} id={materialId} scheduleId={schedule_id} userId={user_id} />}</Box>;
 }
 
 LiveH5p.routeBasePath = "/live-h5p";
