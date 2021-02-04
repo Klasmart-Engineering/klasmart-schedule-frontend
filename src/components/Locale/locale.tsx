236a8803570f82b22def7d83035792a3ba0ef38c
juanjuan.qin <juanjuan.qin@badanamu.com.cn>
2021-02-04 11:21:18 +0800

    fix: 修复 safari 白屏问题

--- src/components/Locale/locale.tsx
+++ src/components/Locale/locale.tsx
@@ -9,7 +9,11 @@ export function Locale(props: LocaleProps) {
   const { children } = props;
   const [, refresh] = useReducer((s) => s + 1, 0);
   const locale = localeManager.getLocale();
-  useEffect(() => localeManager.on("change", refresh), []);
+  useEffect(() => {
+    localeManager.on("change", refresh);
+    if (locale !== localeManager.getLocale()) refresh();
+  }, [locale]);
+  console.log("locale, localeManager.intl = ", locale, localeManager.intl);
   if (!locale || !localeManager.intl) return null;
   return (
     <RawIntlProvider key={locale} value={localeManager.intl}>
