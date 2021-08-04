ab20cc8d21f5343cdb3e2cbd52fc4230a0cb14ba
james.jiang@badanamu.com.cn <xi414000>
2021-08-04 15:26:22 +0800

    fix(NKL-1114): update schedule filter shadow

--- src/components/FilterTree/index.tsx
+++ src/components/FilterTree/index.tsx
@@ -388,7 +388,7 @@ function FilterOverall(props: FilterTreeProps) {
         left: "23.1%",
         top: pageY + "px",
         zIndex: 999,
-        boxShadow: "10px 10px 5px #888888",
+        boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
         backgroundColor: "white",
       }}
     >
