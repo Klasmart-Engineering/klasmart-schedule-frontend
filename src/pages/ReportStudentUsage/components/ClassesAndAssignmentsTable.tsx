828ab5814d5b562cf9e5e74e51897d434022943c
whitney <whitney.wang@kidsloop.cn>
2021-09-07 10:11:45 +0800

    fix(NKL-1249): fix Pagination component

--- src/pages/ReportStudentUsage/components/ClassesAndAssignmentsTable.tsx
+++ src/pages/ReportStudentUsage/components/ClassesAndAssignmentsTable.tsx
@@ -2,20 +2,43 @@ import Box from "@material-ui/core/Box";
 import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
 import Paper from "@material-ui/core/Paper";
+import { createStyles, makeStyles, Theme, useTheme } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
 import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import { InfoOutlined } from "@material-ui/icons";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
-import Pagination from "@material-ui/lab/Pagination";
+import LastPageIcon from "@material-ui/icons/LastPage";
 import React, { useState } from "react";
 import attendList from "../../../mocks/attendList.json";
+
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+    tfoot: {
+      "& .MuiTablePagination-spacer": {
+        display: "none",
+      },
+      "& .MuiTablePagination-toolbar": {
+        justifyContent: "center",
+      },
+    },
+  })
+);
 
 interface IUnAttendedListProps {
   studentName: string;
@@ -33,13 +56,62 @@ interface IRowProps {
   unAttendedList: IUnAttendedListProps[];
 }
 
+function TablePaginationActions2(props: TablePaginationActionsProps) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton onClick={handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="next page">
+        {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton onClick={handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="last page">
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
 function Row(props: { row: IRowProps }) {
+  const css = useStyles1();
   const { row } = props;
   const [open, setOpen] = React.useState(false);
-  const [childrenPage, setChildrenPage] = React.useState(1);
-  const [childrenRowsPerPage] = useState(10);
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setChildrenPage(value);
+  const [childrenPage, setChildrenPage] = React.useState(0);
+  const [childrenRowsPerPage, setChildrenRowsPerPage] = useState(10);
+  // const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+  //   setChildrenPage(value);
+  // };
+
+  const handleChangePage2 = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setChildrenPage(newPage);
+  };
+
+  const handleChangeRowsPerPage2 = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setChildrenRowsPerPage(parseInt(event.target.value, 10));
+    setChildrenPage(0);
   };
 
   return (
@@ -85,10 +157,7 @@ function Row(props: { row: IRowProps }) {
                 </TableHead>
                 <TableBody>
                   {(childrenRowsPerPage > 0
-                    ? row.unAttendedList.slice(
-                        (childrenPage - 1) * childrenRowsPerPage,
-                        (childrenPage - 1) * childrenRowsPerPage + childrenRowsPerPage
-                      )
+                    ? row.unAttendedList.slice(childrenPage * childrenRowsPerPage, childrenPage * childrenRowsPerPage + childrenRowsPerPage)
                     : row.unAttendedList
                   ).map((item, idx) => (
                     <TableRow key={idx} style={{ height: "56px" }}>
@@ -101,9 +170,21 @@ function Row(props: { row: IRowProps }) {
                     </TableRow>
                   ))}
                 </TableBody>
+                <TableFooter className={css.tfoot}>
+                  <TablePagination
+                    size="small"
+                    rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                    count={row.unAttendedList.length}
+                    rowsPerPage={childrenRowsPerPage}
+                    page={childrenPage}
+                    onPageChange={handleChangePage2}
+                    onRowsPerPageChange={handleChangeRowsPerPage2}
+                    ActionsComponent={TablePaginationActions2}
+                  />
+                </TableFooter>
               </Table>
             </Box>
-            <div style={{ margin: "20px auto", display: "flex", justifyContent: "center" }}>
+            {/* <div style={{ margin: "20px auto", display: "flex", justifyContent: "center" }}>
               <Pagination
                 count={Math.ceil(row.unAttendedList.length / childrenRowsPerPage)}
                 page={childrenPage}
@@ -112,7 +193,7 @@ function Row(props: { row: IRowProps }) {
                 onChange={handleChange}
                 size="small"
               />
-            </div>
+            </div> */}
           </Collapse>
         </TableCell>
       </TableRow>
@@ -120,12 +201,64 @@ function Row(props: { row: IRowProps }) {
   );
 }
 
-export default function H5pTable() {
-  const [page, setPage] = React.useState(1);
-  const [rowsPerPage] = React.useState(10);
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+}
 
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton onClick={handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="next page">
+        {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton onClick={handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="last page">
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+export default function H5pTable() {
+  const css = useStyles1();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
@@ -181,17 +314,24 @@ export default function H5pTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {(rowsPerPage > 0 ? attendList.slice((page - 1) * rowsPerPage, (page - 1) * rowsPerPage + rowsPerPage) : attendList).map(
-              (row) => (
-                <Row key={row.className} row={row} />
-              )
-            )}
+            {(rowsPerPage > 0 ? attendList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : attendList).map((row) => (
+              <Row key={row.className} row={row} />
+            ))}
           </TableBody>
+          <TableFooter className={css.tfoot}>
+            <TablePagination
+              style={{ padding: "40px 0 10px 0" }}
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              count={attendList.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableFooter>
         </Table>
       </TableContainer>
-      <div style={{ margin: "80px auto 40px auto", display: "flex", justifyContent: "center" }}>
-        <Pagination count={Math.ceil(attendList.length / rowsPerPage)} page={page} color="primary" onChange={handleChange} />
-      </div>
     </div>
   );
 }
