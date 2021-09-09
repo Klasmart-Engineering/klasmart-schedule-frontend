4975e95106570c2e7bb6ab453a77a7ade5cfa083
sherry <sherry.zeng@kidsloop.cn>
2021-09-09 14:40:23 +0800

    fix(NKL-1249): update Pagination

---
+++ src/pages/ReportStudentUsage/components/Pagination.tsx
@@ -0,0 +1,57 @@
+import { createStyles, makeStyles } from "@material-ui/core";
+import { ChevronLeft, ChevronRight, FirstPage, LastPage } from "@material-ui/icons";
+import clsx from "clsx";
+import React from "react";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    pagination: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+    },
+    icon: {
+      cursor: "pointer",
+      padding: "12px",
+      borderRadius: "50%",
+      "&:hover": {
+        background: "#eee",
+      },
+    },
+    iconColor: {
+      color: "#ccc",
+    },
+  })
+);
+
+interface IPagination {
+  page: number;
+  rowsPerPage: number;
+  count: number;
+  onFirstPage: () => void;
+  onAddPage: () => void;
+  onSubPage: () => void;
+  onLastPage: () => void;
+}
+
+export default function Pagination(props: IPagination) {
+  const { page, rowsPerPage, count, onFirstPage, onAddPage, onSubPage, onLastPage } = props;
+  const css = useStyles();
+
+  return (
+    <div className={css.pagination}>
+      <span style={{ marginRight: 6 }}>Total {count} results</span>
+      <FirstPage className={clsx(css.icon, page === 0 && css.iconColor)} onClick={() => page !== 0 && onFirstPage()} />
+      <ChevronLeft className={clsx(css.icon, page === 0 && css.iconColor)} onClick={() => page !== 0 && onSubPage()} />
+      <ChevronRight
+        className={clsx(css.icon, page === Math.floor(count / rowsPerPage) && css.iconColor)}
+        onClick={() => page !== Math.floor(count / rowsPerPage) && onAddPage()}
+      />
+      <LastPage
+        className={clsx(css.icon, page === Math.floor(count / rowsPerPage) && css.iconColor)}
+        onClick={() => page !== Math.floor(count / rowsPerPage) && onLastPage()}
+      />
+    </div>
+  );
+}
