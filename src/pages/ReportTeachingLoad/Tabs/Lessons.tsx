e71161f58e57d3067f1c01fd0b44bc496abc15ae
sherry <sherry.zeng@kidsloop.cn>
2021-09-29 14:48:18 +0800

    feat(NKL-1291): update lesson table

--- src/pages/ReportTeachingLoad/Tabs/Lessons.tsx
+++ src/pages/ReportTeachingLoad/Tabs/Lessons.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { getDurationByDay } from "../../../models/ModelReports";
+import { getListTeacherMissedLessons } from "../../../reducers/report";
 import LessonChart from "../components/LessonChart";
 import LessonTable from "../components/LessonTable";
 import { SelectContext } from "../index";
+import { useDispatch, useSelector } from "react-redux";
+import ReportPagination from "../../../components/ReportPagination/ReportPagination";
+import { RootState } from "../../../reducers";
 
 export interface IState {
   id: string | undefined;
@@ -9,26 +14,54 @@ export interface IState {
 }
 
 export default function () {
+  const dispatch = useDispatch();
+  const { teachers, classes } = useContext(SelectContext);
   const [state, setState] = React.useState<IState>({
-    id: undefined,
+    id: "150de04b-6777-4cab-a381-5f36a9b750ef",
     days: 7,
   });
+  const { listTeacherMissedLessons } = useSelector<RootState, RootState["report"]>((state) => state.report);
+  const [page, setPage] = React.useState(1);
 
   const teacherChange = (id?: string, days?: number) => {
     setState({ id, days: days ? days : 7 });
   };
+  const handleChangePage = React.useMemo(
+    () => (page: number) => {
+      setPage(page);
+    },
+    []
+  );
+
+  React.useEffect(() => {
+    const class_ids = classes.map((item) => item.value);
+    const teacher_id = state.id ? state.id : "";
+    if (!teacher_id) return;
+    dispatch(
+      getListTeacherMissedLessons({
+        metaLoading: true,
+        class_ids,
+        duration: getDurationByDay(state.days),
+        page,
+        page_size: 10,
+        teacher_id,
+      })
+    );
+  }, [dispatch, page, classes, state.id, state.days]);
 
   return (
-    <SelectContext.Consumer>
-      {(value) => {
-        const { teachers, classes } = value;
-        return (
-          <div>
-            <LessonChart teacherChange={teacherChange} teacherIds={teachers} classIds={classes} />
-            {state.id ? <LessonTable state={state} classIds={classes} /> : ""}
-          </div>
-        );
-      }}
-    </SelectContext.Consumer>
+    <div>
+      <LessonChart teacherChange={teacherChange} teacherIds={teachers} classIds={classes} />
+      {state.id && (
+        <div>
+          <LessonTable listTeacherMissedLessons={listTeacherMissedLessons.list} classIds={classes} />
+          <ReportPagination
+            page={page}
+            count={listTeacherMissedLessons?.total ? listTeacherMissedLessons?.total : 0}
+            onChangePage={handleChangePage}
+          />
+        </div>
+      )}
+    </div>
   );
 }
