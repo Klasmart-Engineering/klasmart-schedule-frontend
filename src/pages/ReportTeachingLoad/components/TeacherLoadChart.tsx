e1d90bde6e4378b3ddb75c46a55efc055867fceb
ruyueliu <ext.ruyue.liu@badanamu.com.cn>
2021-10-14 16:40:33 +0800

    fix(NKL-1294): some ui style

--- src/pages/ReportTeachingLoad/components/TeacherLoadChart.tsx
+++ src/pages/ReportTeachingLoad/components/TeacherLoadChart.tsx
@@ -39,7 +39,7 @@ export function TeacherLoadChart(props: { data: HorizontalBarStackDataItem[]; xL
           valueAxiosLabel={t("report_label_teaching_hours")}
           tickValues={[5, 15, 25, 35, 45, 55, 65]}
           gridValues={[10, 20, 30, 40, 50, 60, 70]}
-          labelProps={{ stroke: "black" }}
+          // labelProps={{ stroke: "black" }}
           structSize={structSize(px)}
           renderRect={({ rectProps, index, stackIndex }) => <rect {...rectProps} fill={data[index].value[stackIndex].color} />}
           renderXAxiosLabel={({ formattedValue, ...textProps }) => (
