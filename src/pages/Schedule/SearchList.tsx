e55959988c09017a4ab6d1b7a2db53950ae81fad
GithubBada <jiangyixi_007@yeah.net>
2021-10-27 11:16:19 +0800

    feat(NKL-1365): schedule: search shows incorrect date for duedate data out of today's range

--- src/pages/Schedule/SearchList.tsx
+++ src/pages/Schedule/SearchList.tsx
@@ -102,20 +102,6 @@ const useQuery = () => {
   return { name };
 };
 
-// const timestampToTime = () => {
-//   const date = new Date();
-//   const dateNumFun = (num: number) => (num < 10 ? `0${num}` : num);
-//   const [Y, M, D] = [
-//     date.getFullYear(),
-//     dateNumFun(date.getMonth() + 1),
-//     dateNumFun(date.getDate()),
-//     dateNumFun(date.getHours()),
-//     dateNumFun(date.getMinutes()),
-//     dateNumFun(date.getSeconds()),
-//   ];
-//   return `${Y}-${M}-${D}`;
-// };
-
 interface SearchListProps {
   timesTamp: timestampType;
 }
@@ -149,7 +135,10 @@ export default function SearchList(props: SearchListProps) {
     } else {
       searchScheduleList.forEach((item: EntityScheduleSearchView, index: number) => {
         if (index < number) {
-          if (timeFormat(item.start_at as number, "dateDay") === timeFormat(date.start_at as number, "dateDay")) {
+          if (
+            timeFormat((item.start_at! > 0 ? item.start_at : item.due_at) as number, "dateDay") ===
+            timeFormat((date.start_at! > 0 ? date.start_at : date.due_at) as number, "dateDay")
+          ) {
             flag = false;
             return;
           } else {
@@ -216,7 +205,9 @@ export default function SearchList(props: SearchListProps) {
         <>
           {searchScheduleList.map((item: EntityScheduleSearchView, index: number) => (
             <div key={item.id} className={classes.partItem}>
-              {isTitleSame(item, index) && <h1 className={classes.titleDate}>{timeFormat(item.start_at as number, "dateDay")}</h1>}
+              {isTitleSame(item, index) && (
+                <h1 className={classes.titleDate}>{timeFormat((item.start_at! > 0 ? item.start_at : item.due_at) as number, "dateDay")}</h1>
+              )}
               <Card className={classes.cardItem} onClick={() => previewSchedule((item.id as unknown) as number)}>
                 <h1 className={`${classes.titleDate} ${classes.itemTitle}`}>{item.title}</h1>
                 <Grid container alignItems="center" className={classes.firstLine}>
