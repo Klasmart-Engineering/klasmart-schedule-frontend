d301842781aae098c951386af0fda9975ded08ef
leo.weng <leo.weng@kidsloop.cn>
2021-11-04 16:07:06 +0800

    feat(NLK-1353): permission component bug fix

--- src/components/Permission/Permission.tsx
+++ src/components/Permission/Permission.tsx
@@ -1,36 +1,72 @@
 import React, { ReactNode } from "react";
 import PermissionType from "../../api/PermissionType";
-import { useChoosePermission, usePermission } from "../../hooks/usePermission";
+import { useChoosePermission, useLoadPermission, usePermission } from "../../hooks/usePermission";
 
-export type PermissionResult<V> = V extends PermissionType[] ? Record<string | number | symbol, boolean | undefined> : boolean | undefined;
+export type PermissionResult<V> = V extends PermissionType[] ? Record<string | number, boolean> : boolean;
 
 export interface PermissionProps<V> {
+  debug?: boolean;
   value: V;
-  render?: (value: PermissionResult<V>) => ReactNode;
+  render?: (value: boolean) => ReactNode;
   children?: ReactNode;
 }
-export function Permission<V extends PermissionType | PermissionType[]>(props: PermissionProps<V>) {
-  const { value, render, children } = props;
+const Permission = function Permission<V extends PermissionType | PermissionType[]>(props: PermissionProps<V>) {
+  const { debug, value, render, children } = props;
   const arr = typeof value === "object" ? (value as PermissionType[]) : ([value] as PermissionType[]);
   const perms = usePermission(arr);
   const specificPerms = useChoosePermission(perms, arr);
-
-  //const perm = usePermission(value);
-  if (render) return <>{render(perms as any)}</>;
   const hasPerm = Object.values(specificPerms).every((v) => v);
+  if (debug) {
+    console.log(perms, specificPerms, hasPerm);
+  }
+  //const perm = usePermission(value);
+  if (render) return <>{render(hasPerm)}</>;
+
   return hasPerm ? <>{children}</> : null;
-}
+};
 
 export interface PermissionOrProps {
   value: PermissionType[];
   render?: (value: boolean) => ReactNode;
   children?: ReactNode;
 }
-export function PermissionOr(props: PermissionOrProps) {
+
+const PermissionOr = function PermissionOr(props: PermissionOrProps) {
   const { value, render, children } = props;
   const perms = usePermission(value);
   const specificPerms = useChoosePermission(perms, value);
-  const perm = Object.values(specificPerms).some((v) => v);
-  if (render) return <>{render(perm)}</>;
-  return perm ? <>{children}</> : null;
+  const hasPerm = Object.values(specificPerms).some((v) => v);
+  if (render) return <>{render(hasPerm)}</>;
+  return hasPerm ? <>{children}</> : null;
+};
+
+export interface PermissionsWrapperProps {
+  value?: PermissionType[];
+  children: React.ReactNode;
 }
+/*
+ This component is used as a pre load for permissions , to send all children‘s permissions request only once 
+*/
+
+const PermissionsWrapper = ({ value: propsValue, children }: PermissionsWrapperProps) => {
+  //const values: PermissionType[] = propsValue ? propsValue : React.Children.map(children, x => (x as any).props.value) as PermissionType[];
+  let values: PermissionType[] = [];
+
+  if (propsValue) {
+    values = propsValue;
+  } else {
+    React.Children.forEach(children, (x) => {
+      const v = (x as any).props.value as PermissionType | PermissionType[];
+      if (typeof v === "object") {
+        values.concat(v);
+      } else {
+        values.push(v);
+      }
+    });
+  }
+
+  const loaded = useLoadPermission(values);
+  return loaded ? <>{children}</> : null;
+};
+
+export { Permission, PermissionOr, PermissionsWrapper };
