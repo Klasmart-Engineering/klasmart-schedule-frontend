afb38f951208368c5e21afa3fcfaed54da94900b
ruyueliu <ext.ruyue.liu@badanamu.com.cn>
2021-11-15 15:26:08 +0800

    chore:  remove h5p scripts

--- scripts/contentTypeDemo.js
+++
@@ -1,7 +0,0 @@
-import requireContentType from './contentType.macro';
-
-// const content = requireContentType('asset', 'H5P.MultiChoice-1.14');
-// const content = requireContentType('schema', 'H5P.MultiChoice-1.14');
-const content = requireContentType('language', 'H5P.MultiChoice-1.14', 'zh');
-
-console.log(content);
