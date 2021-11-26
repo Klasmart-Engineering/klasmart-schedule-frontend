6c296de634c894dd25b95f916f101603bd39fd93
ruyueliu <ext.ruyue.liu@badanamu.com.cn>
2021-11-26 14:58:24 +0800

    feat(CMS-161): add post api of validatePdf

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -15,13 +15,13 @@ const proxy = {
       [`^/${process.env.REACT_APP_KO_BASE_API}`]: "",
     },
   },
-  // [`/${process.env.REACT_APP_KO_VALIDATE_PDF_API}`]: {
-  //   changeOrigin: true,
-  //   target: decodeURIComponent(process.env.REACT_APP_KO_VALIDATE_PDF_API),
-  //   pathRewrite: {
-  //     [`^/${process.env.REACT_APP_KO_VALIDATE_PDF_API}`]: "",
-  //   },
-  // },
+  [`/${process.env.REACT_APP_KO_VALIDATE_PDF_API}`]: {
+    changeOrigin: true,
+    target: decodeURIComponent(process.env.REACT_APP_KO_VALIDATE_PDF_API),
+    pathRewrite: {
+      [`^/${process.env.REACT_APP_KO_VALIDATE_PDF_API}`]: "",
+    },
+  },
   [`/${process.env.REACT_APP_H5P_API}`]: {
     changeOrigin: true,
     target: decodeURIComponent(process.env.REACT_APP_H5P_API),
