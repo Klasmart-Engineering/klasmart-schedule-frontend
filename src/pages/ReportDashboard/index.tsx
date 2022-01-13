bd6199737e3cf2bdba8b43ebcfa1093add99011e
wengcan <wengcan@wengcandeiMac.local>
2022-01-13 14:40:25 +0800

    feat(KLR-174): add permission to report layout

--- src/pages/ReportDashboard/index.tsx
+++ src/pages/ReportDashboard/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Grid, Link, makeStyles, Tooltip, Typography } from "@mater
 import { Theme, withStyles } from "@material-ui/core/styles";
 import { Info, InfoOutlined, KeyboardBackspace } from "@material-ui/icons";
 import { ReportLearningSummary } from "@pages/ReportLearningSummary";
+import ReportStudentProgress from "@pages/ReportStudentProgress";
 import ReportTeachingLoad from "@pages/ReportTeachingLoad";
 import clsx from "clsx";
 import React, { useCallback } from "react";
@@ -10,7 +11,6 @@ import { useHistory } from "react-router";
 import { Link as RouterLink } from "react-router-dom";
 import PermissionType from "../../api/PermissionType";
 import LayoutBox from "../../components/LayoutBox";
-import { permissionTip } from "../../components/TipImages";
 import { usePermission } from "../../hooks/usePermission";
 import { d, t } from "../../locale/LocaleManager";
 import { actSetLoading } from "../../reducers/loading";
@@ -50,11 +50,12 @@ const useStyles = makeStyles(({ shadows, breakpoints }) => ({
     "& > ul": {
       margin: 0,
       padding: 0,
+      paddingTop: 30,
       "& > li": {
         listStyle: "none",
         paddingBottom: 14,
         "& > a": {
-          width: "calc(100% -48px)",
+          width: "calc(100% - 48px)",
           padding: 24,
           lineHeight: 1,
           display: "block",
@@ -65,6 +66,7 @@ const useStyles = makeStyles(({ shadows, breakpoints }) => ({
           fontWeight: 600,
           textDecoration: "none",
           position: "relative",
+
           "&::after": {
             content: "''",
             display: "block",
@@ -80,6 +82,7 @@ const useStyles = makeStyles(({ shadows, breakpoints }) => ({
           },
           "&:hover": {
             background: "#4A7ABE",
+            textDecorationLine: "none",
           },
         },
       },
@@ -150,6 +153,12 @@ interface ReportItem {
 }
 */
 
+interface ReportItem {
+  hasPerm: boolean;
+  label: string;
+  url: string;
+}
+
 const DiyTooltip = withStyles((theme: Theme) => ({
   tooltip: {
     backgroundColor: theme.palette.common.white,
@@ -171,22 +180,23 @@ export function ReportDashboard() {
     PermissionType.view_my_organizations_reports_612,
     PermissionType.view_my_school_reports_611,
     PermissionType.learning_summary_report_653,
+    // Skills Coverage
+    PermissionType.report_learning_outcomes_in_categories_616,
+    PermissionType.report_organizations_skills_taught_640,
+    PermissionType.report_schools_skills_taught_641,
+    PermissionType.report_my_skills_taught_642,
+    PermissionType.report_my_skills_taught_642,
+    // Learner Usage
     PermissionType.student_usage_report_657,
+    PermissionType.report_organizational_student_usage_654,
+    PermissionType.report_school_student_usage_655,
+    PermissionType.report_teacher_student_usage_656,
+    // list
     PermissionType.student_progress_report_662,
+    PermissionType.learning_summary_report_653,
+    PermissionType.organization_class_achievements_report_626,
+    PermissionType.teachers_classes_teaching_time_report_620,
   ]);
-
-  const [hasPerm, hasSummaryPerm, hasStudentUsagePermission, hasStudentProgressPermission, isPending] = React.useMemo(() => {
-    const hasPerm =
-      perm.view_reports_610 ||
-      perm.view_my_reports_614 ||
-      perm.view_my_organizations_reports_612 ||
-      (perm.view_my_school_reports_611 as boolean);
-    const hasSummaryPerm = perm.learning_summary_report_653 as boolean;
-    const hasStudentUsagePermission = perm.student_usage_report_657 as boolean;
-    const hasStudentProgressPermission = perm.student_progress_report_662 as boolean;
-    const isPending = perm.view_reports_610 === undefined;
-    return [hasPerm, hasSummaryPerm, hasStudentUsagePermission, hasStudentProgressPermission, isPending];
-  }, [perm]);
 
   React.useEffect(() => {
     if (Object.keys(perm).length === 0) {
@@ -195,59 +205,39 @@ export function ReportDashboard() {
       dispatch(actSetLoading(false));
     }
   }, [dispatch, perm]);
-  /*
-  const reportList: ReportItem[] = [
-    {
-      title: "report_label_student_achievement",
-      url: ReportAchievementList.routeBasePath,
-      icon: <SvgIcon component={SaIconUrl}></SvgIcon>,
-      bgColor: "#89c4f9",
-      hasPerm: hasPerm,
-    },
-    {
-      title: "report_label_lo_in_categories",
-      url: ReportCategories.routeBasePath,
-      icon: <CategoryOutlined />,
-      bgColor: "#77dcb7",
-      hasPerm: hasPerm,
-    },
-    {
-      title: "report_label_teaching_load",
-      url: ReportTeachingLoad.routeBasePath,
-      icon: <AccessTime />,
-      bgColor: "#ffa966",
-      hasPerm: hasPerm,
-    },
-    {
-      title: "report_learning_summary_report",
-      url: ReportLearningSummary.routeRedirectDefault,
-      icon: <AssignmentTurnedInOutlined />,
-      bgColor: "#FE9494",
-      hasPerm: hasSummaryPerm,
-    },
-    {
-      title: "report_student_usage_report",
-      url: ReportStudentUsage.routeBasePath,
-      icon: <ShowChart />,
-      bgColor: "#DCCDFF",
-      hasPerm: hasStudentUsagePermission,
-    },
-    {
-      title: "report_label_student_progress_report",
-      url: ReportStudentProgress.routeBasePath,
-      icon: <ShortText />,
-      bgColor: "#607d8b",
-      hasPerm: hasStudentProgressPermission,
-    },
-  ];
 
-  const handleClick = useMemo(
-    () => (value: string) => {
-      history.push(value);
-    },
-    [history]
-  );
-    */
+  const [hasSkillCoveragePerm, hasLearnerUsagePerm, hasReportListPerm, reportList, isPending] = React.useMemo(() => {
+    const hasSkillCoveragePerm = !!perm.report_learning_outcomes_in_categories_616;
+    const hasLearnerUsagePerm = !!perm.student_usage_report_657;
+    const reportList: ReportItem[] = [
+      {
+        hasPerm: !!perm.student_progress_report_662,
+        label: t("report_label_learner_usage_info"),
+        url: ReportStudentProgress.routeBasePath,
+      },
+      {
+        hasPerm: !!perm.learning_summary_report_653,
+        label: t("report_learning_summary_report"),
+        url: ReportLearningSummary.routeRedirectDefault,
+      },
+      {
+        hasPerm: !!perm.organization_class_achievements_report_626,
+        label: t("report_label_student_achievement"),
+        url: ReportAchievementList.routeBasePath,
+      },
+      {
+        hasPerm: !!perm.teachers_classes_teaching_time_report_620,
+        label: t("report_label_student_achievement"),
+        url: ReportTeachingLoad.routeBasePath,
+      },
+    ].filter((item) => item.hasPerm);
+    const hasReportListPerm = reportList.length > 0;
+    const isPending = perm.report_learning_outcomes_in_categories_616 === undefined;
+    return [hasSkillCoveragePerm, hasLearnerUsagePerm, hasReportListPerm, reportList, isPending];
+  }, [perm]);
+
+  const hasPerm = hasSkillCoveragePerm || hasLearnerUsagePerm || hasReportListPerm;
+
   return (
     <Box className={css.layoutBoxWrapper}>
       <LayoutBox holderMin={40} holderBase={202} mainBase={1517}>
@@ -256,123 +246,96 @@ export function ReportDashboard() {
         </div>
       </LayoutBox>
       <LayoutBox holderMin={40} holderBase={202} mainBase={1517} className={css.layoutBoxMain}>
-        <Grid container spacing={7}>
-          <Grid item xs={4}>
-            <Box className={clsx(css.gridItem, css.gridItemWithBg)}>1</Box>
-          </Grid>
-          {hasStudentUsagePermission && (
-            <Grid item xs={4}>
-              <div className={css.reportTop}>
-                {t("report_label_learner_usage")}
-                <Tooltip
-                  arrow
-                  placement="bottom"
-                  title={t("report_label_learner_usage_info")}
-                  classes={css}
-                  aria-label="info"
-                  style={{
-                    position: "relative",
-                    left: "7px",
-                    top: "3px",
-                    fontSize: "19px",
-                    color: "#6D8199",
-                    width: "15px",
-                    height: "15px",
-                  }}
-                >
-                  <Info></Info>
-                </Tooltip>
-              </div>
-              <Box className={clsx(css.gridItem, css.gridItemWithBg)}>
-                <LearnerUsageReport />
-              </Box>
-            </Grid>
-          )}
-          <>
-            {(hasPerm || hasSummaryPerm || hasStudentUsagePermission || hasStudentProgressPermission) && (
+        {!isPending && !hasPerm && (
+          <svg width="122" height="148" viewBox="0 0 122 148" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.73486 28.4156L13.2516 31.1752" stroke="#FFBA08" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M28.0273 3.7291L30.6867 13.2606" stroke="#FFBA08" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12.6689 14.7672L20.0941 21.2707" stroke="#FFBA08" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+            <path
+              d="M72.6828 28.6244C65.9001 28.6244 48.6891 28.6245 41.8167 28.9378C34.9443 29.251 31.0151 33.2784 30.2382 37.1417C29.4614 41.0051 29.0581 73.0901 29.0581 84.3072V90.2738C29.0581 101.491 29.4464 133.531 30.2382 137.439C31.0301 141.347 34.9443 145.24 41.8167 145.628C48.6891 146.016 65.9001 146.091 72.6828 146.091C79.4656 146.091 100.411 146.091 107.284 145.777C114.156 145.464 118.086 141.437 118.862 137.573C119.243 134.17 119.453 130.75 119.49 127.326C119.49 127.043 119.49 126.759 119.49 126.476V123.194C119.669 116.168 119.789 107.815 119.863 100.82L120.013 92.0189C120.013 91.1239 120.013 89.513 120.013 87.3948C120.013 85.2767 120.013 83.6658 120.013 82.7708L119.863 73.9701C119.863 70.3753 119.774 66.4225 119.714 62.4547L91.3279 28.6692C84.0521 28.6393 76.3282 28.6244 72.6828 28.6244Z"
+              fill="#F7F7F7"
+              stroke="#41A6DF"
+              stroke-width="2.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M94.9736 57.0252C93.4982 56.3662 92.202 55.3644 91.1936 54.1033C90.1852 52.8421 89.4937 51.3583 89.1769 49.7759C88.7156 46.3592 88.8775 42.8874 89.6549 39.5283C89.9388 37.7533 90.2377 35.9931 90.5365 34.218C90.671 33.4275 91.9258 29.236 91.4925 28.6991L119.878 62.4846C119.311 61.8134 115.83 61.709 114.978 61.5449L108.36 60.3216C104.087 59.5012 98.9178 59.0091 94.9736 57.0252Z"
+              fill="#41A6DF"
+              stroke="#41A6DF"
+              stroke-width="2.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M74.1772 65.7213C67.1447 65.7212 60.3978 68.4992 55.4097 73.4486C50.4216 78.3981 47.5976 85.1171 47.5542 92.1383H74.4462V65.7363L74.1772 65.7213Z"
+              fill="#46ADE8"
+            />
+            <path
+              d="M47.4942 92.1383C47.4942 92.1383 47.4942 92.2427 47.4942 92.3024C47.4878 98.65 49.762 104.789 53.9035 109.605L74.3263 92.1383H47.4942Z"
+              fill="#2689DD"
+            />
+            <path
+              d="M100.785 92.1383C100.742 85.1545 97.9465 78.4685 93.003 73.5273C88.0596 68.5861 81.3661 65.7871 74.3713 65.7363V92.1383L53.9482 109.605C57.4873 113.735 62.2094 116.682 67.4778 118.05C72.7462 119.417 78.3078 119.14 83.4133 117.254C88.5188 115.369 92.9227 111.966 96.0315 107.504C99.1404 103.043 100.805 97.7373 100.8 92.3024C100.803 92.2473 100.798 92.1921 100.785 92.1383Z"
+              fill="#67C1ED"
+            />
+          </svg>
+        )}
+        {!isPending && hasPerm && (
+          <Grid container spacing={7}>
+            {hasSkillCoveragePerm && (
               <Grid item xs={4}>
-                <Box className={clsx(css.gridItem, css.navContainer)}>
-                  <ul>
-                    <li style={{ marginTop: "30px" }}>
-                      <Link component={RouterLink} to={ReportAchievementList.routeBasePath}>
-                        Learner Monthly Report
-                      </Link>
-                    </li>
-                    <li>
-                      <Link component={RouterLink} to={ReportAchievementList.routeBasePath}>
-                        Learner Weekly Report
-                      </Link>
-                    </li>
-                    <li>
-                      <Link component={RouterLink} to={ReportLearningSummary.routeRedirectDefault}>
-                        Learning Outcome Report
-                      </Link>
-                    </li>
-                    <li>
-                      <Link component={RouterLink} to={ReportTeachingLoad.routeBasePath}>
-                        Teacher Usage Report
-                      </Link>
-                    </li>
-                  </ul>
+                <Box className={clsx(css.gridItem, css.gridItemWithBg)}>1</Box>
+              </Grid>
+            )}
+            {hasLearnerUsagePerm && (
+              <Grid item xs={4}>
+                <div className={css.reportTop}>
+                  {t("report_label_learner_usage")}
+                  <Tooltip
+                    arrow
+                    placement="bottom"
+                    title={t("report_label_learner_usage_info")}
+                    classes={css}
+                    aria-label="info"
+                    style={{
+                      position: "relative",
+                      left: "7px",
+                      top: "3px",
+                      fontSize: "19px",
+                      color: "#6D8199",
+                      width: "15px",
+                      height: "15px",
+                    }}
+                  >
+                    <Info></Info>
+                  </Tooltip>
+                </div>
+                <Box className={clsx(css.gridItem, css.gridItemWithBg)}>
+                  <LearnerUsageReport />
                 </Box>
               </Grid>
             )}
-          </>
-        </Grid>
-
-        {isPending ? (
-          ""
-        ) : hasPerm || hasSummaryPerm ? (
-          <>
-            {/*
-          <Hidden smDown>
-            <div className={css.reportList}>
-              {reportList.map(
-                (item) =>
-                  item.hasPerm && (
-                    <div key={item.title} className={css.reportItem} onClick={() => handleClick(item.url)}>
-                      <div className={css.iconBox} style={{ backgroundColor: item.bgColor }}>
-                        {cloneElement(item.icon, { style: { fontSize: 42 } })}{" "}
-                      </div>
-                      <div className={css.reportItemTitleBox}>
-                        <Typography className={css.reportItemTitle}>{t(item.title)}</Typography>
-                        <ChevronRight style={{ opacity: 0.54 }} />
-                      </div>
-                    </div>
-                  )
+            <>
+              {hasReportListPerm && (
+                <Grid item xs={4}>
+                  <Box className={clsx(css.gridItem, css.navContainer)}>
+                    <ul>
+                      {reportList.map((item, index) => {
+                        return (
+                          <li key={index}>
+                            <Link component={RouterLink} to={item.url}>
+                              {item.label}
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </Box>
+                </Grid>
               )}
-            </div>
-          </Hidden>
-          <Hidden mdUp>
-
-
-
-
-            <Grid container spacing={4}>
-              {reportList.map(
-                (item) =>
-                  item.hasPerm && (
-                    <Grid key={item.title} item xs={6} className={css.gridCon}>
-                      <div className={css.reportItemMb} onClick={() => handleClick(item.url)}>
-                        <div style={{ display: "flex", justifyContent: "center" }}>
-                          <div className={css.iconBox} style={{ backgroundColor: item.bgColor }}>
-                            {cloneElement(item.icon, { style: { fontSize: 22 } })}{" "}
-                          </div>
-                        </div>
-                        <div className={css.reportItemTitleBox}>
-                          <Typography className={css.reportItemTitle}>{t(item.title)}</Typography>
-                          <ChevronRight style={{ opacity: 0.54, marginTop: 7 }} />
-                        </div>
-                      </div>
-                    </Grid>
-                  )
-              )}
-            </Grid>
-          </Hidden>
-                  */}
-          </>
-        ) : (
-          permissionTip
+            </>
+          </Grid>
         )}
       </LayoutBox>
     </Box>
