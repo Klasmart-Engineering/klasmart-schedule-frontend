dccc243f7b947c1c92874aa24f787eaee278af03
James.jiang <jiangyixi_007@yeah.net>
2022-02-22 11:38:17 +0800

    feat(KLS-297): optimise 'teacher search' page view to be mobile responsive

--- src/pages/Schedule/SearchList.tsx
+++ src/pages/Schedule/SearchList.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
+import { Box, Card, createStyles, Grid, makeStyles, useMediaQuery, useTheme } from "@material-ui/core";
 import { AccessTime, PeopleOutlineOutlined } from "@material-ui/icons";
 import { Pagination } from "@material-ui/lab";
 import React from "react";
@@ -12,10 +12,13 @@ import { getSearchScheduleList } from "../../reducers/schedule";
 import { timestampType } from "../../types/scheduleTypes";
 import Tooltip from "@material-ui/core/Tooltip/Tooltip";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(({ breakpoints }) =>
   createStyles({
     listContainer: {
       padding: "20px 40px 10px 40px",
+      [breakpoints.down(1400)]: {
+        padding: "8px 8px 0px 0px",
+      },
       boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.16)",
       borderRadius: "8px",
     },
@@ -40,6 +43,9 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: "30px",
       verticalAlign: "middle",
       marginRight: "15px",
+      [breakpoints.down(1400)]: {
+        fontSize: "20px",
+      },
     },
     timeItem: {
       display: "inline-block",
@@ -58,6 +64,13 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paginationUl: {
       justifyContent: "center",
+    },
+    searchItemMb: {
+      padding: "22px",
+      "& span": {
+        display: "block",
+        color: "#FFFFFF",
+      },
     },
   })
 );
@@ -182,7 +195,7 @@ export default function SearchList(props: SearchListProps) {
   };
 
   const textEllipsis = (value?: string) => {
-    const CharacterCount = 40;
+    const CharacterCount = mobile ? 36 : 40;
     return value ? reBytesStr(value, CharacterCount) : "";
   };
 
@@ -191,61 +204,109 @@ export default function SearchList(props: SearchListProps) {
     const temp = item.map((teacherItem: EntityScheduleShortInfo) => teacherItem.name);
     const list = temp.join(", ");
     return (
-      <span>
+      <span style={{ display: "inline" }}>
         <Tooltip title={list} placement="top-start">
-          <span>{textEllipsis(list)}</span>
+          <span style={{ display: "inline" }}>{textEllipsis(list)}</span>
         </Tooltip>{" "}
         ({student_num})
       </span>
     );
   };
 
+  const { breakpoints } = useTheme();
+  const mobile = useMediaQuery(breakpoints.down(600));
+
+  const eventColorMb = [
+    { id: "OnlineClass", color: "#0E78D5" },
+    { id: "OfflineClass", color: "#1BADE5" },
+    { id: "Homework", color: "#13AAA9" },
+    { id: "Task", color: "#AFBA0A" },
+  ];
+
+  const CustomEvent = (id?: string) => {
+    const eventTemplate = eventColorMb.filter((item) => item.id === id);
+    return id ? eventTemplate[0].color : "#009688";
+  };
+
   return (
     <Box className={classes.listContainer}>
       {searchScheduleList && searchScheduleList.length > 0 ? (
-        <>
-          {searchScheduleList.map((item: EntityScheduleSearchView, index: number) => (
-            <div key={item.id} className={classes.partItem}>
-              {isTitleSame(item, index) && (
-                <h1 className={classes.titleDate}>{timeFormat((item.start_at! > 0 ? item.start_at : item.due_at) as number, "dateDay")}</h1>
-              )}
-              <Card className={classes.cardItem} onClick={() => previewSchedule(item.id as unknown as number)}>
-                <h1 className={`${classes.titleDate} ${classes.itemTitle}`}>{item.title}</h1>
-                <Grid container alignItems="center" className={classes.firstLine}>
-                  <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
-                    <span className={classes.timeItem}>
-                      <AccessTime className={classes.clockIcon} />
-                    </span>
-                    <span className={classes.timeItem}>
-                      {timeFormat(item.start_at as number, "timeOnly")} - {timeFormat(item.end_at as number, "timeOnly")}
-                    </span>
+        mobile ? (
+          <Box>
+            {searchScheduleList.map((item: EntityScheduleSearchView, index: number) => (
+              <div style={{ padding: "0px 16px 0px 16px" }}>
+                {isTitleSame(item, index) && (
+                  <p style={{ fontSize: "16px", fontWeight: 700 }}>
+                    {timeFormat((item.start_at! > 0 ? item.start_at : item.due_at) as number, "dateDay")}
+                  </p>
+                )}
+                <div
+                  className={classes.searchItemMb}
+                  style={{ backgroundColor: CustomEvent(item.class?.type), borderRadius: "12px", marginTop: "10px" }}
+                >
+                  <span style={{ fontSize: "20px", fontWeight: 700 }} onClick={() => previewSchedule(item.id as unknown as number)}>
+                    {item.title}
+                  </span>
+                  <span style={{ fontSize: "14px", fontWeight: 400, marginTop: "20px" }}>
+                    <AccessTime className={classes.clockIcon} /> {timeFormat(item.start_at as number, "timeOnly")} -{" "}
+                    {timeFormat(item.end_at as number, "timeOnly")}{" "}
+                  </span>
+                  <span style={{ fontSize: "14px", fontWeight: 400, marginTop: "20px" }}>
+                    <PeopleOutlineOutlined className={classes.clockIcon} /> {getTeacherList(item.member_teachers, item.student_count)}{" "}
+                  </span>
+                  <span style={{ fontSize: "12px", fontWeight: 300, marginTop: "20px" }}>Jess_testing_Lesson_Plan</span>
+                  <span style={{ fontSize: "12px", fontWeight: 300, marginTop: "6px" }}>None Specified</span>
+                </div>
+              </div>
+            ))}
+          </Box>
+        ) : (
+          <>
+            {searchScheduleList.map((item: EntityScheduleSearchView, index: number) => (
+              <div key={item.id} className={classes.partItem}>
+                {isTitleSame(item, index) && (
+                  <h1 className={classes.titleDate}>
+                    {timeFormat((item.start_at! > 0 ? item.start_at : item.due_at) as number, "dateDay")}
+                  </h1>
+                )}
+                <Card className={classes.cardItem} onClick={() => previewSchedule(item.id as unknown as number)}>
+                  <h1 className={`${classes.titleDate} ${classes.itemTitle}`}>{item.title}</h1>
+                  <Grid container alignItems="center" className={classes.firstLine}>
+                    <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
+                      <span className={classes.timeItem}>
+                        <AccessTime className={classes.clockIcon} />
+                      </span>
+                      <span className={classes.timeItem}>
+                        {timeFormat(item.start_at as number, "timeOnly")} - {timeFormat(item.end_at as number, "timeOnly")}
+                      </span>
+                    </Grid>
+                    <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+                      <span className={classes.timeItem}>{item?.lesson_plan?.name}</span>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
-                    <span className={classes.timeItem}>{item?.lesson_plan?.name}</span>
+                  <Grid container alignItems="center">
+                    <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
+                      <span className={classes.timeItem}>
+                        <PeopleOutlineOutlined className={classes.clockIcon} />
+                      </span>
+                      <span className={classes.timeItem}>{getTeacherList(item.member_teachers, item.student_count)}</span>
+                    </Grid>
+                    <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+                      <span className={classes.timeItem}>{item?.program?.name}</span>
+                    </Grid>
                   </Grid>
-                </Grid>
-                <Grid container alignItems="center">
-                  <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
-                    <span className={classes.timeItem}>
-                      <PeopleOutlineOutlined className={classes.clockIcon} />
-                    </span>
-                    <span className={classes.timeItem}>{getTeacherList(item.member_teachers, item.student_count)}</span>
-                  </Grid>
-                  <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
-                    <span className={classes.timeItem}>{item?.program?.name}</span>
-                  </Grid>
-                </Grid>
-              </Card>
-            </div>
-          ))}
-        </>
+                </Card>
+              </div>
+            ))}
+          </>
+        )
       ) : (
         <div style={{ textAlign: "center" }}>
           <img src={emptyBox} alt="" />
           <p>{d("No results found.").t("schedule_msg_no_result")}</p>
         </div>
       )}
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
         <Pagination
           count={Math.ceil(total / 10)}
           color="primary"
