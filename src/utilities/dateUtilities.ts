5bdedee47c4d7fc515c527a789fc2f901e2ec820
ruyue16 <Liury16@163.com>
2022-03-10 11:27:50 +0800

    feat(KLR-121): report DisplayRealTimeData

--- src/utilities/dateUtilities.ts
+++ src/utilities/dateUtilities.ts
@@ -2,13 +2,10 @@ import moment from "moment";
 
 export function getFourWeeks() {
   const now = moment();
-  const day = now.isoWeekday();
   const startOfThisWeek = moment(now).startOf("isoWeek");
   let res = [];
-  if (day !== 1) {
-    res.push(`${startOfThisWeek.unix()}-${moment(now).startOf("day").unix()}`);
-  }
-  const weeksArr = Array(day === 1 ? 4 : 3).fill(null);
+  res.push(`${startOfThisWeek.unix()}-${moment(now).unix()}`);
+  const weeksArr = Array(3).fill(null);
   res = res.concat(
     weeksArr.map(() => {
       return `${startOfThisWeek.subtract(7, "days").unix()}-${startOfThisWeek.clone().endOf("isoWeek").add(1, "second").unix()}`;
@@ -18,8 +15,10 @@ export function getFourWeeks() {
 }
 
 export function getDurationByDay(day: number) {
-  const currentDate = moment().startOf("day").unix();
-  return `${currentDate - 3600 * 24 * day}-${currentDate}`;
+  return `${moment()
+    .subtract(day - 1, "days")
+    .startOf("day")
+    .unix()}-${moment().unix()}`;
 }
 
 export function getTimeDots() {
@@ -36,14 +35,10 @@ export function getTimeDots() {
 }
 
 export function getLastedMonths(index: number) {
-  const day = moment().get("date");
   const startOfThisMonth = moment().startOf("months");
   let res = [];
-  index === 3 && res.push(`${startOfThisMonth.unix()}-${moment().unix()}`);
-  if (day !== 1 && index !== 3) {
-    res.push(`${startOfThisMonth.unix()}-${moment().startOf("date").unix()}`);
-  }
-  const monthsArr = Array(day === 1 && index !== 3 ? index : index - 1).fill(null);
+  res.push(`${startOfThisMonth.unix()}-${moment().unix()}`);
+  const monthsArr = Array(index - 1).fill(null);
   res = res.concat(
     monthsArr.map(() => {
       return `${startOfThisMonth.subtract(1, "months").unix()}-${startOfThisMonth.clone().endOf("months").add(1, "second").unix()}`;
