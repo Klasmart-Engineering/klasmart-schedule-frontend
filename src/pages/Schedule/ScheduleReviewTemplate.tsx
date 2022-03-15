6afd601d90364596b94374dc733debed00893489
james.jiang@badanamu.com.cn <xi414000>
2022-03-15 20:35:36 +0800

    feat(KLS-317): bug fix for review module

--- src/pages/Schedule/ScheduleReviewTemplate.tsx
+++ src/pages/Schedule/ScheduleReviewTemplate.tsx
@@ -74,11 +74,12 @@ interface InfoProps {
     is_force: boolean,
     is_new_schedule: boolean,
     is_check_review: boolean
-  ) => void
+  ) => void;
+  disabledConfirm: boolean
 }
 
 export default function ScheduleReviewTemplate(props: InfoProps) {
-  const { handleClose, checkScheduleReviewData, saveSchedule } = props;
+  const { handleClose, checkScheduleReviewData, saveSchedule, disabledConfirm } = props;
   const css = useStyles();
   const { breakpoints } = useTheme();
   const mobile = useMediaQuery(breakpoints.down(600));
@@ -86,12 +87,21 @@ export default function ScheduleReviewTemplate(props: InfoProps) {
   return (
     <div className={css.reviewBox}>
       <div className={css.title}></div>
-      <p>
-        {d("The schedule will appear on the calendar once the system completes publishing a personalized Lesson Plan for each student.").t("schedule_review_pop_up_all_success")}{" "}
-      </p>
-      <p>
-        {d("The following students will receive a random Lesson Plan as they have not enough data during the time range selected. To avoid this, let your student working on at least 10 activities before setting a review session:").t("schedule_review_popup_partical_success")}
-      </p>
+      {
+        disabledConfirm && <p style={{color: "red"}}>
+          {d("There were not enough content covered for this group of students during the date range specified to create a review session. Please try again with a different date range or student group.").t("schedule_review_group_fail_no_data")}{" "}
+        </p>
+      }
+      {
+        !disabledConfirm && <>
+          <p>
+            {d("The schedule will appear on the calendar once the system completes publishing a personalized Lesson Plan for each student.").t("schedule_review_pop_up_all_success")}{" "}
+          </p>
+          <p>
+            {d("The following students will receive a random Lesson Plan as they have not enough data during the time range selected. To avoid this, let your student working on at least 10 activities before setting a review session:").t("schedule_review_popup_partical_success")}
+          </p>
+        </>
+      }
       <div className={css.checkboxContainer}>
         {
           checkScheduleReviewData?.usersConnection?.edges?.map((item: any)=>{
@@ -103,7 +113,7 @@ export default function ScheduleReviewTemplate(props: InfoProps) {
         <Button variant="outlined" onClick={handleClose}>
           {d("Cancel").t("assess_button_cancel")}
         </Button>
-        <Button variant="contained" color="primary" className={css.lastButton} onClick={()=>{
+        <Button variant="contained" color="primary" disabled={disabledConfirm} className={css.lastButton} onClick={()=>{
           saveSchedule("only_current", true, false, false)
           handleClose()
         }}>
