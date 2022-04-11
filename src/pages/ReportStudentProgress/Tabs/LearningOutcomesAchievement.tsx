b4a8c20ba8c704ff069d29baef35abbc1ed2fea0
ruyue16 <Liury16@163.com>
2022-04-11 12:43:44 +0800

    fix(KLR-223): report  add judgement condition before dispatch

--- src/pages/ReportStudentProgress/Tabs/LearningOutcomesAchievement.tsx
+++ src/pages/ReportStudentProgress/Tabs/LearningOutcomesAchievement.tsx
@@ -78,16 +78,18 @@ export default function LearningOutcomesAchievement() {
   const handleChange = React.useMemo(
     () => (value: number) => {
       setDurationTime(value);
-      dispatch(
-        getLearnOutcomeAchievement({
-          metaLoading: true,
-          class_id: classId,
-          durations: value === 4 ? getFourWeeks() : getLastedMonths(6),
-          selected_subject_id_list: selectedSubjectId,
-          student_id: studentId,
-          un_selected_subject_id_list: unselectedSubjectId,
-        })
-      );
+      if (classId && studentId) {
+        dispatch(
+          getLearnOutcomeAchievement({
+            metaLoading: true,
+            class_id: classId,
+            durations: value === 4 ? getFourWeeks() : getLastedMonths(6),
+            selected_subject_id_list: selectedSubjectId,
+            student_id: studentId,
+            un_selected_subject_id_list: unselectedSubjectId,
+          })
+        );
+      }
     },
     // eslint-disable-next-line
     [classId, selectedSubjectID, studentId]
