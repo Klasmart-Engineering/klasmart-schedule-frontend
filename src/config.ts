d982b579faf82be0e4e13d3a11edf0ba0dcd64a3
James.Jiang <james.jiang@badanumu.com.cn>
2022-06-23 15:38:49 +0800

    feat(KLS-495): widgets component bug fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const getCmsApiEndpoint = () => process.env.CMS_API_ENDPOINT ?? ``;
+export const getCmsApiEndpoint = () => `${process.env.REACT_APP_BASE_DOMAIN}/` ?? ``;
 
 export const getCmsSiteEndpoint = () => process.env.CMS_SITE_ENDPOINT ?? ``;
 
@@ -6,7 +6,7 @@ export const getAuthEndpoint = () => process.env.AUTH_ENDPOINT ?? ``;
 
 export const getAPIEndpoint = () => process.env.API_ENDPOINT ?? ``;
 
-export const getLiveEndpoint = () => process.env.LIVE_ENDPOINT ?? ``;
+export const getLiveEndpoint = () => process.env.REACT_APP_LIVE_LINK ?? ``;
 
 export const getCookieDomain = () => process.env.COOKIE_DOMAIN ?? ``;
 
